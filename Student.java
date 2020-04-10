3105f15310909d1c7966b568304a7d85b898597c
Irina Chabukovska <irina.chabukovska@gmail.com>
2020-04-10 19:14:08 +0200

    grades list added

--- Student.java
+++ Student.java
@@ -1,9 +1,18 @@
-class Student {
+import java.util.ArrayList;
+
+public class Student {
 	String index;
 	String firstName;
 	String lastName;
+	ArrayList<Integer> grades;
 
 	//TODO constructor
+	public Student(String index, String firstName, String lastName, ArrayList<Integer> grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades =  grades;
+	}
 
 	//TODO seters & getters
 
